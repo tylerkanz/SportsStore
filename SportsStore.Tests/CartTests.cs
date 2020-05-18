c40350b6392a55b4fe2c261b92d4c38392e6d589
tylerkanz <tylerkanz@gmail.com>
2020-05-18 11:46:59 -0500

    initial commit

---
+++ SportsStore.Tests/CartTests.cs
@@ -0,0 +1,107 @@
+﻿using System.Linq;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests {
+
+    public class CartTests {
+
+        [Fact]
+        public void Can_Add_New_Lines() {
+
+            // Create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            // ACreate a new cart
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            CartLine[] results = target.Lines.ToArray();
+
+            Assert.Equal(2, results.Length);
+            Assert.Equal(p1, results[0].Product);
+            Assert.Equal(p2, results[1].Product);
+        }
+
+        [Fact]
+        public void Can_Add_Quantity_For_Existing_Lines() {
+            // Create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+
+            // Create a new cart
+            Cart target = new Cart();
+
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 10);
+            CartLine[] results = target.Lines
+                .OrderBy(c => c.Product.ProductID).ToArray();
+
+            Assert.Equal(2, results.Length);
+            Assert.Equal(11, results[0].Quantity);
+            Assert.Equal(1, results[1].Quantity);
+        }
+
+        [Fact]
+        public void Can_Remove_Line() {
+            // Create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1" };
+            Product p2 = new Product { ProductID = 2, Name = "P2" };
+            Product p3 = new Product { ProductID = 3, Name = "P3" };
+
+            // Create a new cart
+            Cart target = new Cart();
+            // Add some products to the cart
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 3);
+            target.AddItem(p3, 5);
+            target.AddItem(p2, 1);
+
+            // Act
+            target.RemoveLine(p2);
+
+            Assert.Equal(0, target.Lines.Where(c => c.Product == p2).Count());
+            Assert.Equal(2, target.Lines.Count());
+        }
+
+        [Fact]
+        public void Calculate_Cart_Total() {
+            // Create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            // Create a new cart
+            Cart target = new Cart();
+			
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+            target.AddItem(p1, 3);
+            decimal result = target.ComputeTotalValue();
+
+            Assert.Equal(450M, result);
+        }
+
+        [Fact]
+        public void Can_Clear_Contents() {
+            // Create some test products
+            Product p1 = new Product { ProductID = 1, Name = "P1", Price = 100M };
+            Product p2 = new Product { ProductID = 2, Name = "P2", Price = 50M };
+
+            // Create a new cart
+            Cart target = new Cart();
+
+            // Add some items
+            target.AddItem(p1, 1);
+            target.AddItem(p2, 1);
+
+            // Reset the cart
+            target.Clear();
+
+            Assert.Equal(0, target.Lines.Count());
+        }
+
+    }
+}
---
+++ SportsStore.Tests/OrderControllerTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Models;
+using Xunit;
+
+namespace SportsStore.Tests {
+
+    public class OrderControllerTests {
+
+        [Fact]
+        public void Cannot_Checkout_Empty_Cart() {
+            // Create a mock repository
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+            // Create an empty cart
+            Cart cart = new Cart();
+            // Create the order
+            Order order = new Order();
+            // Create an instance of the controller
+            OrderController target = new OrderController(mock.Object, cart);
+
+            ViewResult result = target.Checkout(order) as ViewResult;
+
+            // Check that the order hasn't been stored 
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+            // Check that the method is returning the default view
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+            // Check that I am passing an invalid model to the view
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        public void Cannot_Checkout_Invalid_ShippingDetails() {
+
+            // Create a mock order repository
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+            // Create a cart with one item
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            // Create an instance of the controller
+            OrderController target = new OrderController(mock.Object, cart);
+            // Add an error to the model
+            target.ModelState.AddModelError("error", "error");
+
+            // Try to checkout
+            ViewResult result = target.Checkout(new Order()) as ViewResult;
+
+            // Check that the order hasn't been passed stored
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Never);
+            // Check that the method is returning the default view
+            Assert.True(string.IsNullOrEmpty(result.ViewName));
+            // Check that I am passing an invalid model to the view
+            Assert.False(result.ViewData.ModelState.IsValid);
+        }
+
+        [Fact]
+        public void Can_Checkout_And_Submit_Order() {
+            // Create a mock order repository
+            Mock<IOrderRepository> mock = new Mock<IOrderRepository>();
+            // Create a cart with one item
+            Cart cart = new Cart();
+            cart.AddItem(new Product(), 1);
+            // Create an instance of the controller
+            OrderController target = new OrderController(mock.Object, cart);
+
+            // Try to checkout
+            RedirectToActionResult result =
+                 target.Checkout(new Order()) as RedirectToActionResult;
+
+            // Check that the order has been stored
+            mock.Verify(m => m.SaveOrder(It.IsAny<Order>()), Times.Once);
+            // Check that the method is redirecting to the Completed action
+            Assert.Equal("Completed", result.ActionName);
+        }
+
+    }
+}
